f722abe086830a8bfca8e60f53d9a7b4ec6ebf45
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-21 18:39:50 +0300

    ecsirse of list'

--- loop.py
+++ loop.py
@@ -2,24 +2,14 @@
 
 ## for loop
 #list of friends
-friends = ["Abdullahi", "Ali", "Abdi","Hassan", "Rashid"]
-friends.append("Sadio Mane")
-friends.append("Mo salah")
-# greet every friend in the list
-for friend in friends:
-    print(f"Hello {friend}")
 
 # adding each letter in Abdullahi  in a list
 # variable with my name
-name = "Abdullahi"
-# an empty list
-letters_in_abdullahi = []
-
 # for loop to get each letter in abdullahi,
 # and add it to letters_in_abdullahi variable
-for letter in name:
-    letters_in_abdullahi.append(letter.title())
-    print(letters_in_abdullahi)
+#for letter in name:
+ #   letters_in_abdullahi.append(letter.title())
+#    print(letters_in_abdullahi)
 
 
 # get each numer in a range of numeber
@@ -29,5 +19,12 @@ for letter in name:
 # for number in numbers:
 #     print("hello")
 
-for number in range(1,5):
-    print(number)
+#for number in range(1,5):
+#    print(number)
+
+#ecsirse
+
+cars = ["lanbagini", "brado", "bandrom"]
+cars.append("ferari")    
+for car in cars:
+    print(f"my car is {car}")
