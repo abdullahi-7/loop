5febf9696fbe9e44d003e92c89e24d4907eba247
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-21 18:03:39 +0300

    adding each letter in Abdullahi in a list

--- loop.py
+++ loop.py
@@ -8,3 +8,15 @@ friends.append("Mo salah")
 # greet every friend in the list
 for friend in friends:
     print(f"Hello {friend}")
+
+# adding each letter in Abdullahi  in a list
+# variable with my name
+name = "Abdullahi"
+# an empty list
+letters_in_abdullahi = []
+
+# for loop to get each letter in abdullahi,
+# and add it to letters_in_abdullahi variable
+for letter in name:
+    letters_in_abdullahi.append(letter.title())
+    print(letters_in_abdullahi)
