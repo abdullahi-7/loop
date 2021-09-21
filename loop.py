a41ee0ae6b8e99cb468690f0fd8efa97f5f164bf
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-21 17:25:08 +0300

    Greeting list of friends using for loop

--- loop.py
+++ loop.py
@@ -1,2 +1,10 @@
 ## Loops : there are two loops: for and while loop
+
 ## for loop
+#list of friends
+friends = ["Abdullahi", "Ali", "Abdi","Hassan", "Rashid"]
+friends.append("Sadio Mane")
+friends.append("Mo salah")
+# greet every friend in the list
+for friend in friends:
+    print(f"Hello {friend}")
