fdca4d0cf85bda2ff80eab94bbc35d62063e0cfc
abdullahi-7 <abdullahi.abdi@protonmail.com>
2021-09-25 06:46:38 +0300

    while loop

--- while_loop.py
+++ while_loop.py
@@ -1,8 +1,18 @@
 # a fuction in  has a loop when we call fuction we call in the loop
 def loop():
     for number in range (20,100):
-     print(number)
-letters = "abdullahi"
-letters_in_abdullahi = []
-for letter in letters:
-    letters_in_abdullahi.append(letter)
+        print(number)
+
+
+    letters = "abdullahi"
+    letters_in_abdullahi = []
+
+    for letter in letters:
+        print(letters_in_abdullahi.append(letter))
+
+
+    name = 5
+
+    while name > 0:
+        name = name -1
+        print(f"hellow{name}")
